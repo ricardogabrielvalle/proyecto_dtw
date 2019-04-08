4aabfcdf18c43e7c7cd2dee327d474215f252378
Ricardo Valle <ricardogabrielvalle@gmail.com>
2019-04-08 15:43:10 -0500

    Creacion del archivo index.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Control de versiones</title>
+</head>
+<body>
+
+<h1>Control de versiones</h1>
+<p>Página index de práctica de control de versiones</p>
+
+</body>
+</html>
