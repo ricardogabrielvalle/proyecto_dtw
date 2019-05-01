4fc7305a4d85fb665f39d4e9fba30e907fe76469
Ricardo Valle <ricardogabrielvalle@gmail.com>
2019-05-01 16:51:16 -0500

    Inicio de proyecto
    Página principal añadida, barra de navegación y descripción en el index.

--- index.php
+++ index.php
@@ -1,13 +1,33 @@
 <!DOCTYPE html>
+
 <html>
-<head>
-<title>Control de versiones</title>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <title>MINJAL</title>
+    </head>
+    <body>
+        <?php include 'navbar.php';//barra de navegación y bootstrap ?>
+        
+        <!-- Información con fines demostrativos -->
+        <h1>Minería de datos Jalisco, Investigación y análisis en beneficio de la población</h1>
+        <p>
+            Somos un equipo de investigación dedicados a encontrar soluciones a los problemas de la población, utilizando las técncicas de minería de datos. Miles de datos se generan cada día, por si solos no nos dicen mucho, pero dentro de todos los datos generados se encuentrar patrones que a simple vista no se pueden ver, en los cuales pueden estar las claves y/o soluciones a diferentes problemas que enfrenta la sociedad.
+            Las áreas más importantes son la salud, la seguridad y la educación, si nuestro estado puede detectar los problemas, podemos empezar a solucionarlas y mejorar la calidad de vida de todos los jaliscienses.
+        </p>
+        <h1>¿Qué es la minería de datos? </h1>
+        <p>
+            La minería de datos es el proceso de seleccionar, explorar, modificar, modelar y valorar grandes cantidades de datos con el objetivo de descubrir patrones desconocidos que pueden ser utilizados como soporte de toma de decisiones utilizando herramientas de análisis de datos.
+        </p>
+        <!-- Footer -->
+        <footer class="page-footer font-small blue fixed-bottom">
+          <!-- Copyright -->
+          <div class="footer-copyright text-center py-3">
+              © 2019 Copyright
+              <a href="index.php"> Minería de datos Jalisco</a>
+          </div>
+          <!-- Copyright -->
 
-<h1>Control de versiones</h1>
-<p>Página index de práctica de control de versiones</p>
-  <p> HOLA MUNDO </p>
-
-</body>
+        </footer>
+        <!-- Footer -->
+    </body>
 </html>
---
+++ navbar.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    </head>
+    <!-- Barra de navegación -->
+    <body style="height:1500px">
+        <div class="container">
+            <img src="img/logo_jalisco.png" class="img-fluid" alt="...">
+        </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">MINJAL</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Análisis e investigaciones</a>
+              </li>
+                <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
+              <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Buscar</button>
+            </form>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <a class="btn btn-outline-dark my-2 my-sm-0" href="#" role="button">Iniciar Sesión</a>
+            </form>
+          </div>
+        </nav>
+        
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    </body>
+</html>
